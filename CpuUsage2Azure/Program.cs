925cbcc929663e7a32076fffdb5bafa49929aff3
snobu <foo@snobu.org>
2018-03-19 01:21:02 -0700

    Code edits

--- CpuUsage2Azure/Program.cs
+++ CpuUsage2Azure/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 using System.Diagnostics;
 using Microsoft.Azure.Devices.Client;
 using System.Timers;
@@ -25,7 +21,7 @@ namespace CpuUsage2Azure
 
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Running...");
             // We need to use decimal number using . as separator
             System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
 
@@ -38,10 +34,10 @@ namespace CpuUsage2Azure
             _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             _ramCounter = new PerformanceCounter("Memory", "Available MBytes");
 
-            // read CPU usage every second
-            _timer = new Timer(1000);
+            // read CPU usage every x seconds
+            _timer = new Timer(9 * 1000); // every 9 seconds
             _timer.AutoReset = true;
-            _timer.Elapsed += _timer_Elapsed;
+            _timer.Elapsed += SendTelemetry;
             _timer.Start();
 
             // application runs until closed
@@ -50,7 +46,7 @@ namespace CpuUsage2Azure
             }
         }
 
-        private static async void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        private static async void SendTelemetry(object sender, ElapsedEventArgs e)
         {
             // Get % of CPU usage, decimal number
             float cpuUsage = _cpuCounter.NextValue();
@@ -58,7 +54,7 @@ namespace CpuUsage2Azure
             float ramUsage = _ramCounter.NextValue();
             
             // create payload for IoT Hub
-            string payload = String.Format("{{'c':{0},'fr':{1}}}", cpuUsage, ramUsage);
+            string payload = String.Format("{{'cpu_usage':{0},'memory_usage':{1}}}", cpuUsage, ramUsage);
             Console.WriteLine(payload);
             
             // prepare message
